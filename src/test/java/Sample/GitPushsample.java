62879a69ed54d8f55268dd06fdc7607dfa99bfec
dileep <dileepkkd4u@gmail.com>
2025-01-11 13:09:16 +0530

    My Second Commit with some print message change

--- src/test/java/Sample/GitPushsample.java
+++ src/test/java/Sample/GitPushsample.java
@@ -1,5 +1,15 @@
 package Sample;
 
+import org.testng.annotations.Test;
+
 public class GitPushsample {
 
+	@Test
+	public void sample()
+	{
+		System.out.println("Git Practice");
+	    System.out.println("Hellow");
+	    System.out.println("My second commit")
+		
+	}
 }
